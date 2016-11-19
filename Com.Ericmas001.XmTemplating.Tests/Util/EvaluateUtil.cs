805c7b6d44dc8b935b58acf7915e9b52dc923774
Éric Massé <Ericmas001@gmail.com>
2016-11-19 18:30:40 -0500

    TestingDefines

--- Com.Ericmas001.XmTemplating.Tests/Util/EvaluateUtil.cs
+++ Com.Ericmas001.XmTemplating.Tests/Util/EvaluateUtil.cs
@@ -10,49 +10,69 @@ namespace Com.Ericmas001.XmTemplating.Tests.Util
 {
     internal static class EvaluateUtil
     {
-        public static void EvaluateBooleanCondition(bool expectedResult, string condition, IDictionary<string, string> vars, Dictionary<string, IEnumerable<string>> arrays = null)
+        public static void EvaluateBooleanCondition(bool expectedResult, string condition, IDictionary<string, string> vars, IDictionary<string, IEnumerable<string>> arrays = null)
         {
             EvaluateExpression(expectedResult.ToString(), condition, vars, arrays);
             EvaluateIf(expectedResult, condition, vars, arrays);
         }
-        public static void EvaluateIf(bool expectedResult, string condition, IDictionary<string, string> vars, Dictionary<string, IEnumerable<string>> arrays = null)
+        public static void EvaluateIf(bool expectedResult, string condition, IDictionary<string, string> vars, IDictionary<string, IEnumerable<string>> arrays = null)
         {
             Evaluate(expectedResult.ToString(), "<IF ", $">{true}<:ELSE:>{false}</IF>", condition, vars, arrays);
         }
-        public static void EvaluateExpression(string expectedResult, string expression, IDictionary<string, string> vars, Dictionary<string, IEnumerable<string>> arrays = null)
+        public static void EvaluateExpression(string expectedResult, string expression, IDictionary<string, string> vars, IDictionary<string, IEnumerable<string>> arrays = null)
         {
             Evaluate(expectedResult, "<EVAL ", " />", expression, vars, arrays);
         }
-        public static void EvaluateFor(string expectedResult, string condition, string content, IDictionary<string, string> vars, Dictionary<string, IEnumerable<string>> arrays = null)
+        public static void EvaluateFor(string expectedResult, string condition, string content, IDictionary<string, string> vars, IDictionary<string, IEnumerable<string>> arrays = null)
         {
             Evaluate(expectedResult, "<FOR ", ">" + content + "</FOREACH>", condition, vars, arrays);
         }
-        public static void EvaluateForeach(string expectedResult, string condition, string content, IDictionary<string, string> vars, Dictionary<string, IEnumerable<string>> arrays = null)
+        public static void EvaluateForeach(string expectedResult, string condition, string content, IDictionary<string, string> vars, IDictionary<string, IEnumerable<string>> arrays = null)
         {
             Evaluate(expectedResult, "<FOREACH ", ">" + content + "</FOREACH>", condition, vars, arrays);
         }
-
-        public static void Evaluate(string expectedResult, string beforeCondition, string afterCondition, string condition, IDictionary<string, string> vars, Dictionary<string, IEnumerable<string>> arrays = null)
+        public static void EvaluateDefine(string expected, string templateStr)
         {
             //Arrange
+            var sw = new StringWriter();
+
+            //Act
+            var result = EvaluateUtil.ExecuteTemplate(templateStr, sw);
+
+            //Assert
+            Assert.AreEqual(expected, result, sw.ToString());
+        }
+
+        public static void Evaluate(string expectedResult, string beforeCondition, string afterCondition, string condition, IDictionary<string, string> vars, IDictionary<string, IEnumerable<string>> arrays = null)
+        {
+            //Arrange
+            var sw = new StringWriter();
             var templateStr = $"{beforeCondition}[{condition}]{afterCondition}";
+
+            //Act
+            var result = ExecuteTemplate(templateStr, sw, vars, arrays);
+            
+            //Assert
+            Assert.AreEqual(expectedResult, result, sw.ToString());
+        }
+
+
+        public static string ExecuteTemplate(string templateStr, StringWriter sw, IDictionary<string, string> variables = null, IDictionary<string, IEnumerable<string>> arrays = null)
+        {
             var template = XmTemplateDeserializer.Deserialize(templateStr);
 
             //Act
             var arr = arrays ?? new Dictionary<string, IEnumerable<string>>();
+            var vars = variables ?? new Dictionary<string, string>();
             var result = XmTemplateSerializer.Serialize(template, vars, arr);
 
-            var sw = new StringWriter();
             sw.WriteLine();
             sw.WriteLine(templateStr);
-            sw.WriteLine(string.Join(Environment.NewLine, vars.Select(x => $"{x.Key} = {x.Value}")));
+            foreach (var x in vars)
+                sw.WriteLine("{0} = {1}", x.Key, x.Value);
             foreach (var array in arr)
-            {
                 sw.WriteLine("{0} = ({1})", array.Key, string.Join(", ", array.Value));
-            }
-
-            //Assert
-            Assert.AreEqual(expectedResult, result, sw.ToString());
+            return result;
         }
     }
 }
---
+++ Com.Ericmas001.XmTemplating.Tests/VariablesTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Com.Ericmas001.XmTemplating.Deserialization;
+using Com.Ericmas001.XmTemplating.Serialization;
+using Com.Ericmas001.XmTemplating.Tests.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Com.Ericmas001.XmTemplating.Tests
+{
+    [TestClass]
+    public class VariablesTests
+    {
+        [TestMethod]
+        public void TestDefine()
+        {
+            var expectedResult = "COOL!";
+
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE {Var}>" + expectedResult + "</DEFINE>{Var}");
+
+            //Redefine
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE {Var}>NOT COOL</DEFINE><DEFINE {Var}>" + expectedResult + "</DEFINE>{Var}");
+
+            //Redefine in subscope is ignored
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE {Var}>" + expectedResult + "</DEFINE><IF [\"True\"]><DEFINE {Var}>NOT COOL</DEFINE></IF>{Var}");
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE {Var}>" + expectedResult + "</DEFINE><FOR [{I} From \"1\" To \"5\"]><DEFINE {Var}>NOT COOL</DEFINE></FOR>{Var}");
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE {Var}>" + expectedResult + "</DEFINE><FOREACH [{X} IN (\"A\",\"B\",\"C\")]><DEFINE {Var}>NOT COOL</DEFINE></FOREACH>{Var}");
+        }
+        [TestMethod]
+        public void TestDefineArray()
+        {
+            var stuff = new[] { "A", "B", "C", "D", "E", "F" };
+            var expectedResult = string.Concat(stuff);
+
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE_ARRAY {Vars}>" + string.Join(Environment.NewLine, stuff) + "</DEFINE_ARRAY><FOREACH {Vars}>{Vars}</FOREACH>");
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE_ARRAY {Vars}>" + Environment.NewLine + string.Join(Environment.NewLine, stuff) + Environment.NewLine + "</DEFINE_ARRAY><FOREACH {Vars}>{Vars}</FOREACH>");
+
+            //Redefine
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE_ARRAY {Vars}>" + string.Concat(stuff.Reverse()) + "</DEFINE_ARRAY><DEFINE_ARRAY {Vars}>" + string.Join(Environment.NewLine, stuff) + "</DEFINE_ARRAY><FOREACH {Vars}>{Vars}</FOREACH>");
+
+            //Redefine in subscope is ignored
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE_ARRAY {Vars}>" + string.Join(Environment.NewLine, stuff) + "</DEFINE_ARRAY><IF [\"True\"]><DEFINE_ARRAY {Vars}>" + string.Concat(stuff.Reverse()) + "</DEFINE_ARRAY></IF><FOREACH {Vars}>{Vars}</FOREACH>");
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE_ARRAY {Vars}>" + string.Join(Environment.NewLine, stuff) + "</DEFINE_ARRAY><FOR [{I} From \"1\" To \"5\"]><DEFINE_ARRAY {Vars}>" + string.Concat(stuff.Reverse()) + "</DEFINE_ARRAY></FOR><FOREACH {Vars}>{Vars}</FOREACH>");
+            EvaluateUtil.EvaluateDefine(expectedResult, "<DEFINE_ARRAY {Vars}>" + string.Join(Environment.NewLine, stuff) + "</DEFINE_ARRAY><FOREACH [{X} IN (\"A\",\"B\",\"C\")]><DEFINE_ARRAY {Vars}>" + string.Concat(stuff.Reverse()) + "</DEFINE_ARRAY></FOREACH><FOREACH {Vars}>{Vars}</FOREACH>");
+        }
+    }
+}
